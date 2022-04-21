6f74ce745460ae7a86937e7c2dae18ec1f6b8089
mohamedslima <hammaslima@gmail.com>
2022-04-21 19:44:49 +0200

    completed exercise 1

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -12,3 +12,34 @@ console.log("parola divisa in lettere", splitUserWord);
 let splitUserWordReverse = splitUserWord.reverse();
 console.log("lettere inserite al contrario", splitUserWordReverse);
 
+// unisco la parola al contrario
+
+let userWordReverse = splitUserWordReverse.join("");
+console.log(userWordReverse);
+
+// variabile 
+
+if (isThisWordPalindromes(userWord, userWordReverse)){
+    alert ("la parola che hai inserito è palindroma");
+} else {
+    alert ("la parola che hai inserito non è palindroma");
+}
+
+// funzione
+
+/**
+ * Description
+ * @param {string} - firstWord
+ * @param {string} - secondWord
+ * @returns {boolean} - valore booleano
+ */
+
+function isThisWordPalindromes(firstWord, secondWord) {
+    let comparison = false;
+
+    if (firstWord === secondWord) {
+        comparison = true;
+    }
+
+    return comparison;
+}
